39c69fe2bf80dce46567894063ac4822d74811ba
quantudev211154 <quantu211154@gmail.com>
2022-04-26 14:41:02 +0700

    Add Login Fragment

--- app/src/main/java/com/hisu/androidteamproject/MainActivity.java
+++ app/src/main/java/com/hisu/androidteamproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.hisu.androidteamproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout frmContainer = findViewById(R.id.frmFragmentContainer);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(frmContainer.getId(), new LoginFragment())
+                .commit();
     }
 }
